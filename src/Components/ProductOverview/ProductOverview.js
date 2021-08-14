78bf6365ecd69b58389ed6848615970470f5fd13
Xue Li <xueli@Xues-MBP.lan>
2021-08-14 23:14:56 +0100

    update route to product page

--- src/Components/ProductOverview/ProductOverview.js
+++ src/Components/ProductOverview/ProductOverview.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
 import Placeholder from "./product-image-placeholder.jpg";
 
 export const ProductOverview = React.memo(({ 
@@ -15,6 +16,8 @@ export const ProductOverview = React.memo(({
         alt: ""
     });
     const [isLoading, setIsLoading] = useState(true);
+
+    const { categoryName, productName } = useParams();
 
     useEffect(() => {
         const imageToLoad = new Image();
@@ -38,38 +41,40 @@ export const ProductOverview = React.memo(({
         <button 
             className="product-overview-container"
             id={name + "5"}
-            onClick={handleProductClick}
+            // onClick={handleProductClick}
             onMouseEnter={handleMouseEnter}
             onMouseLeave={handleMouseLeave}
             style={{backgroundColor: currentCategory === null | currentCategory === "all" ? "white" : "#F2F1F0"}}
-            >
-            <div className="flip-image">
-                <img 
-                    src={displayImg.src} 
-                    alt={displayImg.alt}
-                    id={name + "1"}
-                    className="display-image"
-                    style={{
-                        opacity: isLoading ? 0.5 : 1,
-                        transition: "opacity .15s linear"
-                      }}
-                ></img>
-                {window.screen.width >= 600 && <div 
-                    id={name + "2"}
-                    className="quick-view-btn"
-                >Quick View
-                </div>}
-            </div>
-            <h3 
-                id={name + "3"}
-                className="product-overview-title"
-                >{name}
-            </h3>
-            <p 
-                id={name + "4"}
-                className="product-overview-text"
-                >£{price}.00
-            </p>
+        >
+            <Link to={"/category/" + categoryName + "/product/" + name.toLowerCase().replaceAll(" ", "_")}>
+                <div className="flip-image">
+                    <img 
+                        src={displayImg.src} 
+                        alt={displayImg.alt}
+                        id={name + "1"}
+                        className="display-image"
+                        style={{
+                            opacity: isLoading ? 0.5 : 1,
+                            transition: "opacity .15s linear"
+                        }}
+                    ></img>
+                    {window.screen.width >= 600 && <div 
+                        id={name + "2"}
+                        className="quick-view-btn"
+                    >Quick View
+                    </div>}
+                </div>
+                <h3 
+                    id={name + "3"}
+                    className="product-overview-title"
+                    >{name}
+                </h3>
+                <p 
+                    id={name + "4"}
+                    className="product-overview-text"
+                    >£{price}.00
+                </p>
+            </Link>
         </button>
     )
 })
